c1ff9317c4f256fc834e28280fbb94c33c8d95a7
BigBrain <safa7saf7@gmail.com>
2022-02-23 12:04:23 +0400

    new

---
+++ _next/static/xfP59Iq7EJeudcv8gazg7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/459-337f0999c9f772d3.js","static/chunks/pages/index-33d73d1d2580a964.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
